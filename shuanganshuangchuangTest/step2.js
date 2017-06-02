71451692356a760c6ea0714912c5c894713c4b84
shadowforcex <95892680@qq.com>
2017-06-02 15:35:10 +0800

    22

--- shuanganshuangchuangTest/step2.js
+++ shuanganshuangchuangTest/step2.js
@@ -8,7 +8,7 @@ var setupVal={
 
 $(document).ready(function(){
 
-    alert(1.1);
+    alert(1.2);
 
 
     bgLoad("#bgImg1");
@@ -83,15 +83,15 @@ function orientationHandler(event) {
     console.log(outterWidth);
     var mid= (innerWidth-outterWidth)/2;
 
-    if(-mid+event.gamma*setupVal.speed<(-2*mid)  ){
+    if(-mid+event.alpha*setupVal.speed<(-2*mid)  ){
 
         $(".onShow").find(".imgPart").css("left",-2*mid);
     }
-   else if(-mid+event.gamma*setupVal.speed>0){
+   else if(-mid+event.alpha*setupVal.speed>0){
 
         $(".onShow").find(".imgPart").css("left",0);
     }else{
-        $(".onShow").find(".imgPart").css("left",-mid+event.gamma*setupVal.speed);
+        $(".onShow").find(".imgPart").css("left",-mid+event.alpha*setupVal.speed);
 
     }
   //  $(".onShow").find(".imgPart").css("left",(-mid+event.alpha*setupVal.speed<(-2*mid)?-2*mid:-mid+event.alpha*setupVal.speed>0?0:-mid+event.alpha*setupVal.speed)+"px");
